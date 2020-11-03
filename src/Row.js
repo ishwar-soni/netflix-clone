3b79b522d6cacfb424d10b3b45bfe77024d306a0
Ishwar Soni <ishwar.soni@upgrad.com>
2020-11-03 13:21:40 +0530

    add all the componenets

---
+++ src/Row.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchURL, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  //   console.log(movies);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      console.log("movie", movie);
+      movieTrailer(movie?.name || movie?.original_name || "")
+        .then((url) => {
+          console.log("url", url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            src={`${BASE_IMAGE_URL}${
+              isLargeRow
+                ? movie.poster_path
+                : movie.backdrop_path || movie.poster_path
+            }`}
+            alt={movie.name || movie.title}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
